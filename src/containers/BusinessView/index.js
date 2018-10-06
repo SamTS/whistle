30b18d20fcb8802cc39c43f11310c5ac1ac12b54
Mark Muskardin <mark.muskardin@gmail.com>
2018-10-06 14:30:51 -0700

    changed some container views

---
+++ src/containers/BusinessView/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BusinessView extends Component {
+  render() {
+    return (
+      <div className="container">Business-related proofs</div>
+    )
+  }
+}
+
+export default BusinessView
---
+++ src/containers/PoliticsView/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PoliticsView extends Component {
+  render() {
+    return (
+      <div className="container">Politics-related proofs</div>
+    )
+  }
+}
+
+export default PoliticsView
