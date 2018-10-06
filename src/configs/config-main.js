3c0097f48f20d3e18d06814d45b31c0856542ccc
Mark Muskardin <mark.muskardin@gmail.com>
2018-10-06 10:58:23 -0700

    added front-end architecture

---
+++ src/configs/config-main.js
@@ -0,0 +1,8 @@
+/* Main app configs go here */
+
+export const appConfig = {
+  name: 'Crypto Sources',
+  shortName: 'CryptoSources',
+  description: 'Allow whistleblowers to demonstrate their value without giving away who they are',
+  splashScreenBackground: '#ffffff'
+}
