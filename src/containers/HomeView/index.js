2bc10205e801865f2e4a7c18c65c3c156e7af25b
Mark Muskardin <mark.muskardin@gmail.com>
2018-10-06 11:05:45 -0700

    updated configs

--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { appConfig }        from 'configs/config-main'
 
 class HomeView extends Component {
   render() {
     return (
-      <div className="container">Welcome to React Dapp Boilerplate!</div>
+      <div className="container">Welcome to {appConfig.name}!</div>
     )
   }
 }
